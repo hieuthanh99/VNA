a45a3680685f82f604a8f98a5460b9565af5a682
thanhnh <thanhnh@aht-asia.com>
2023-07-21 04:44:24 +0700

    Push

--- app/Http/Controllers/DashBoardController.php
+++ app/Http/Controllers/DashBoardController.php
@@ -12,6 +12,8 @@ use App\Models\Task;
 use App\Models\Logs;
 use Illuminate\Support\Facades\Session;
 use App\Models\ReportCenter;
+use App\Mail\SendReportEmail;
+use Illuminate\Support\Facades\Mail;
 
 class DashBoardController extends Controller
 {
@@ -26,6 +28,7 @@ class DashBoardController extends Controller
         $user = Auth::user();
         if($user->role == 'admin'){
             $report = ReportCenter::all();
+          
             return view('dashboard', ['reports' => $report, 'department' => $department, 'array' => []]);
         }else{
             $departmentUser = Department::find($user->department);
@@ -35,5 +38,87 @@ class DashBoardController extends Controller
             $array = Logs::Where('department_id', $departmentUser->id)->whereBetween('created_at', [$startDate, $endDate])->first();
             return view('dashboard', ['array' => $array, 'department' => $department, 'reports' => []]);
         }     
+    }
+    
+    public function search(Request $request)
+    {
+        $department = Department::get()->toArray();
+        $user = Auth::user();
+        $departmentUser = Department::find($user->department);
+        $startDate = Carbon::parse($request->start_date);
+        $endDate = Carbon::parse($request->end_date);
+        $array = Logs::Where('department_id', $departmentUser->id)->whereBetween('created_at', [$startDate, $endDate])->get();
+
+      //  dd($array);
+        return view('dashboard', ['array' => $array, 'department' => $department, 'reports' => []]);
+    }
+    public function run()
+    {
+        try {
+
+            $startDate = Carbon::now()->startOfWeek();
+            $endDate = Carbon::now()->endOfWeek();
+            $endDate2 = Carbon::now()->setISODate(Carbon::now()->year, Carbon::now()->isoWeek(), 5)->setTime(16, 0, 0);
+
+            $reportCenter = ReportCenter::whereBetween('created_at', [$startDate, $endDate2])->get();
+            
+            if(!$reportCenter->isEmpty()){
+                return redirect()->back()->with('error', 'Bạn đã thực thi trong tuần này.');
+            }
+
+            $records = Logs::whereBetween('created_at', [$startDate, $endDate])->get();
+            if ($records->count() > 0) {
+                $dataByDepartment = [];
+            
+                foreach ($records as $record) {
+                    $values = json_decode($record->values, true);
+                
+                    $departmentId = $record->department_id;
+                
+                    // Tạo một mảng mới cho phòng ban nếu chưa tồn tại
+                    if (!isset($dataByDepartment[$departmentId])) {
+                        $departmentName = Department::find($departmentId);
+                        $dataByDepartment[$departmentId] = [
+                            'DepartmentId' => $departmentId,
+                            'DepartmentName' => $departmentName->name,
+                            'WorkDone' => [],
+                            'ExpectedWork' => [],
+                            'Request' => null,
+                        ];
+                    }
+                    // Tổng hợp dữ liệu từ WorkDone
+                    if (isset($values['WorkDone'])) {
+                        $dataByDepartment[$departmentId]['WorkDone'] = array_merge($dataByDepartment[$departmentId]['WorkDone'], $values['WorkDone']);
+                    }
+                    // Tổng hợp dữ liệu từ ExpectedWork
+                    if (isset($values['ExpectedWork'])) {
+                        $dataByDepartment[$departmentId]['ExpectedWork'] = array_merge($dataByDepartment[$departmentId]['ExpectedWork'], $values['ExpectedWork']);
+                    }
+                    // Lưu giá trị từ Request (nếu có)
+                    if (isset($values['Request'])) {
+                        $dataByDepartment[$departmentId]['Request'] = $values['Request'];
+                    }
+                }
+                $jsonData = json_encode(array_values($dataByDepartment));
+                ReportCenter::create([
+                    'values' => $jsonData,
+                    'created_at' => $endDate2,
+                ]);
+                \Log::info("Testing Cron is Running ... !".$jsonData);
+                \Log::info('Daily report has been sent successfully!');
+                //return 'Daily report has been sent successfully!';
+                $pdfData = 'Đây là nội dung của PDF';
+                $emailArray = User::pluck('email')->filter(function ($email) {
+                    return filter_var($email, FILTER_VALIDATE_EMAIL);
+                })->toArray();
+                
+                Mail::to($emailArray)->send(new SendReportEmail($pdfData));
+                return redirect()->back()->with('success', 'Thực thi thành công báo cáo và email.');
+            }
+            \Log::info("Not Fonnd Report ");
+            return redirect()->back()->with('error', 'Chưa có phòng ban nào báo cáo.');
+        } catch (QueryException $e) {
+            return redirect()->back()->with('error', $e);
+        }
     }
 }
--- app/Http/Controllers/ReportCenterController.php
+++ app/Http/Controllers/ReportCenterController.php
@@ -7,6 +7,7 @@ use App\Models\ReportCenter;
 use App\Models\Logs;
 use Carbon\Carbon;
 use App\Models\Department;
+use App\Helpers\Helper;
 
 class ReportCenterController extends Controller
 {
@@ -17,45 +18,14 @@ class ReportCenterController extends Controller
      */
     public function index()
     {
-        $startDate = Carbon::now()->startOfWeek();
-        $endDateWeek = Carbon::now()->endOfWeek();
-        $endDate = Carbon::now()->setISODate(Carbon::now()->year, Carbon::now()->isoWeek(), 5)->setTime(17, 0, 0);
-        $department = Department::get()->toArray();
-        $data = ReportCenter::whereBetween('created_at', [$startDate, $endDate])->value('values');
-        $data = json_decode($data, true) ?? [];
-        $mergedArray = [];
-        $id = 0;
-        foreach ($department as $dept) {
-            $departmentId = $dept['id'];
-        
-            $item = array_filter($data, function ($value) use ($departmentId) {
-                return $value['DepartmentId'] == $departmentId;
-            });
-        
-            if (count($item) > 0) {
-                $mergedArray[] = [
-                    'DepartmentId' => $item[$id]['DepartmentId'],
-                    'DepartmentName' => $dept['name'],
-                    'WorkDone' => $item[$id]['WorkDone'],
-                    'ExpectedWork' => $item[$id]['ExpectedWork'],
-                    'Request' => $item[$id]['Request']
-                ];
-            } else {
-                $mergedArray[] = [
-                    'DepartmentId' => $departmentId,
-                    'DepartmentName' => $dept['name'],
-                    'WorkDone' => [],
-                    'ExpectedWork' => [],
-                    'Request' => ''
-                ];
-            }
-            $id++;
-        }
-        
-        
-        if (empty($data)) {
-            return view('centers.index', ['data' => $data, 'startDate' => $startDate->format('d-m-Y'), 'endDate' => $endDateWeek->format('d-m-Y')]);
-        }        
+       
+        $result = Helper::reportWeeked();
+        $mergedArray = $result['mergedArray'] ?? null;
+        $startDate = $result['startDate'] ?? null;
+        $endDateWeek = $result['endDateWeek'] ?? null;
+        // if (empty($mergedArray['WorkDone']) && empty($mergedArray['ExpectedWork'])) {
+        //     return view('centers.index', ['data' => [], 'startDate' => $startDate->format('d-m-Y'), 'endDate' => $endDateWeek->format('d-m-Y')]);
+        // }        
         return view('centers.index', ['data' => $mergedArray, 'startDate' => $startDate->format('d-m-Y'), 'endDate' => $endDateWeek->format('d-m-Y')]);
     }
 
