8bfcb945118267940cbe5a5bdcf7396bf40ed06a
thanhlong <longbt@aht-asia.com>
2023-07-31 16:34:23 +0700

    feature/check-enddate

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,6 +7,8 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 use App\Models\ReportDate;
 use Carbon\Carbon;
 use App\Models\ReportCenter;
+use DateTimeZone;
+use DateTime;
 
 class Kernel extends ConsoleKernel
 {
@@ -25,10 +27,20 @@ class Kernel extends ConsoleKernel
         $reportDate = ReportDate::latest()->first();
         $date = $reportDate->report_date;
         $carbonDate = Carbon::parse($date);
-        $dayOfWeek = $carbonDate->dayOfWeek;
-        $carbonDate->setTime(16, 00);
+        $carbonDate->setTime(16, 34);
+        $tz_from = 'Asia/Ho_Chi_Minh'; 
 
-        $cronExpression = $carbonDate->format("i H * * $dayOfWeek");
+        $newDateTime = new DateTime($carbonDate, new DateTimeZone($tz_from)); 
+
+        $newDateTime->setTimezone(new DateTimeZone("UTC"));
+        
+        $dateTimeUTC = $newDateTime->format("Y-m-d H:i:s");
+        $hour = Carbon::parse($dateTimeUTC)->hour;
+        $minute = Carbon::parse($dateTimeUTC)->minute;
+
+        $dayOfWeek = $carbonDate->dayOfWeek;
+        $cronExpression = "$minute $hour * * $dayOfWeek";
+
         $schedule->command('daily:report')->cron($cronExpression);
     }
 
